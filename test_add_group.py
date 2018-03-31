4304ea8f236f1978852bbe42b5957adefbc422f0
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-03-31 10:56:31 +0300

    driver

--- test_add_group.py
+++ test_add_group.py
@@ -21,6 +21,7 @@ class test_add_group(unittest.TestCase):
         return wd.get("http://addressbook/")
     
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -33,7 +34,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
     
     def create_group(self, wd, group):
-
+        self.open_group_page(wd)
         # init group creation
         wd.find_element_by_name("new").click() 
         # fill group form
@@ -48,7 +49,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-
+        self.retutn_to_group_page(wd)
 
     def retutn_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -58,20 +59,14 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="fdg", header="dffgfgr", footer="dvfvfbbf"))
-        self.retutn_to_group_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.retutn_to_group_page(wd)
         self.logout(wd)
 
     
